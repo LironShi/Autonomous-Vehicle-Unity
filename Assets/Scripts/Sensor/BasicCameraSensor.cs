8916e2269febef4ff06e740ff6405e6be6b7573d
LironShi <spamlirons102@gmail.com>
2025-12-08 13:53:30 +0200

    Basic Sensor Detection Of Objects

---
+++ Assets/Scripts/Sensor/BasicCameraSensor.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+namespace Sensor
+{
+    public class BasicCameraSensor : MonoBehaviour
+    {
+        public float detectionRadius = 20f;
+        public LayerMask detectionLayer;
+        
+        private static readonly int NumberOfObjectsToDetect = 20;
+        private readonly Collider[] _colliders = new Collider[NumberOfObjectsToDetect];
+        
+        private Camera _camera;
+        private Plane[] _frustumPlanes;
+
+
+        private void Awake()
+        {
+            _camera = GetComponent<Camera>();
+        }
+
+        void Update()
+        {
+            _frustumPlanes = GeometryUtility.CalculateFrustumPlanes(_camera);
+            
+            int possibleCollidersInViewCount = Physics.OverlapSphereNonAlloc(
+                _camera.transform.position,
+                detectionRadius,
+                _colliders,
+                detectionLayer);
+
+            for (int i = 0; i < possibleCollidersInViewCount; i++)
+            {
+                var currentCollider = _colliders[i];
+                if (currentCollider == null) continue;
+
+                if (GeometryUtility.TestPlanesAABB(_frustumPlanes, currentCollider.bounds))
+                {
+                    Debug.Log($"Detected in view: {currentCollider.name}", currentCollider.gameObject);
+                }
+            }
+        }
+    }
+}
